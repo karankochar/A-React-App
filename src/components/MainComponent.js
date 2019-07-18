90a60578eb7cbff4941abfe61fb3b3fc8b6639e3
Karan Kochar <karankochar30@gmail.com>
2019-07-18 14:58:51 +0530

    Routes and breadcrumbs added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -20,11 +20,14 @@ class Main extends Component {
         dishes: DISHES,
         comments: COMMENTS,
         promotions: PROMOTIONS,
-        leaders: LEADERS
+        leaders: LEADERS,
+        selectedDish: null
     };
   }
 
-
+onDishSelect(dishId){
+  this.setState({selectedDish: dishId});
+}
   render() {
     
     const HomePage = () => {
@@ -36,18 +39,30 @@ class Main extends Component {
           />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header/>
-        <Switch>
-          <Route path = '/home' component = {HomePage}/>
-          <Route exact path  = '/menu' component = {() => 
-            <Menu dishes = {this.state.dishes}/> }/>
-          <Route exact path = "/contactus" component = {Contact}/>
-          <Redirect to = "/home"/>
-        </Switch>
         
+        
+        <Switch>
+          <Route exact path = '/home' component = {HomePage}/>
+          <Route exact path  = '/menu' component = {() => 
+            <Menu dishes = {this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)}/> }/>
+          <Route exact path = "/contactus" component = {Contact}/>
+          <Route exact path = "/aboutus" component = {() => <About leaders = {this.state.leaders}/> }/>
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Redirect to = "/home"/>
+          </Switch>
+          
+          {/* <DishDetail dish= {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>      */}
         <Footer/>
+        
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay ,CardTitle } from 'reactstrap';
 import './menu.css';
+import { Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 class Menu extends Component {
@@ -17,10 +19,12 @@ class Menu extends Component {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
                 <Card onClick={() => this.props.onClick(dish.id)}>
+                <Link to={`/menu/${dish.id}`} >
                   <CardImg id="images" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
                   </CardImgOverlay>
+                  </Link>
                 </Card>
               </div>
             );
@@ -29,6 +33,16 @@ class Menu extends Component {
 
         return (
             <div className="container">
+               <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>About Us</h3>
+                    <hr />
+                </div>                
+            </div>
                 <div className="row">
                     {menu}
                 </div>
@@ -38,6 +52,7 @@ class Menu extends Component {
                     
                     
                   </div>
+                  
                 </div>
             </div>
         );
